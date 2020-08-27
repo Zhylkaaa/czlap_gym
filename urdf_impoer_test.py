e0389cc47152c150e8f5f8564f3514e5f2e68d44
Krzysztof Wojciechowski <krzy.wojciecho@gmail.com>
2020-08-27 10:33:21 +0200

    PyBullet gravity test

---
+++ urdf_impoer_test.py
@@ -0,0 +1,19 @@
+import pybullet as p
+import time
+import pybullet_data
+
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+print(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF(fileName="plane.urdf")
+boxId = p.loadURDF(fileName="racecar/racecar.urdf",basePosition=[0,0,1])
+# boxId = p.loadURDF(fileName="urdf_model/robot.urdf",basePosition=[0,0,1])
+
+# for i in range(p.getNumJoints(boxId)):
+#     print(p.getJointInfo(boxId, i))
+
+for _ in range (1000):
+    p.stepSimulation()
+    time.sleep(1./240.)
+p.disconnect()
